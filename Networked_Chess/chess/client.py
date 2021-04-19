2d7c0b7e91711bb2bb32c6f568ba49385df4d12c
dtadessse87@gmail.com <davemtadesse@gmail.com>
2021-04-18 21:04:29 -0500

    socket -  multi socket communication

--- Networked_Chess/chess/client.py
+++ Networked_Chess/chess/client.py
@@ -1,18 +1,65 @@
 import socket
+import select
+import errno
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 4321))
-#s.setblocking(False)
+HEADER_LENGTH = 10
+IP = socket.gethostname()
+PORT = 4321
 
+player = input("Player: ")
 
-class Client:
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def newClient(self):
+clientSocket.connect((IP, PORT))
+clientSocket.setblocking(False)
+
+# Prepare playerName and header and send them
+# We need to encode playerName to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+playerName = player.encode('utf-8')
+playerHeader = f"{len(playerName):<{HEADER_LENGTH}}".encode('utf-8')
+clientSocket.send(playerHeader + playerName)
+
+while True:
+
+    # Wait for user to input a message
+    message = input(f'{player} > ')
+
+    # If message is not empty - send it
+    if message:
+    
+        message = message.encode('utf-8')
+        messageHeader = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        clientSocket.send(messageHeader + message)
+
+    try:
         while True:
-            msg = s.recv(1024)
-            print(msg.decode("utf-8"))
-            move = input("Please enter move: ")
-            s.sendto((bytes(move, "utf-8")), (socket.gethostname(), 4321))
 
-if __name__ == '__main__':
-    Client().newClient()
+            playerHeader = clientSocket.recv(HEADER_LENGTH)
+
+            if not len(playerHeader):
+                print('Connection closed by the server')
+                sys.exit()
+
+            playerLength = int(playerHeader.decode('utf-8').strip())
+
+            playerName = clientSocket.recv(playerLength).decode('utf-8')
+
+            messageHeader = clientSocket.recv(HEADER_LENGTH)
+            messageLength = int(messageHeader.decode('utf-8').strip())
+            message = clientSocket.recv(messageLength).decode('utf-8')
+
+            # Print message
+            print(f'{playerName} > {message}')
+
+    except IOError as e:
+        # If we got different error code - something happened
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+
+        # We just did not receive anything
+        continue
+
+    except Exception as e:
+        print('Reading error: '.format(str(e)))
+        sys.exit()
--- Networked_Chess/chess/server.py
+++ Networked_Chess/chess/server.py
@@ -1,23 +1,95 @@
 import socket
 import selectors
+import select
 
+HEADER_LENGTH = 10
+
+IP = socket.gethostname()
+PORT = 4321
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 4321))
-sel = selectors.DefaultSelector()
-#s.setblocking(False)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((IP, PORT))
+s.listen()
+socketsList = [s]
+clients = {}
 
-# below is the function to set the number of clients that can access the
-# server. python documentation can provide us info on how to use it to
-# create a queue of clients and pair into games.
-s.listen(6)
+# Handles message receiving
+def receiveMessage(clientSocket):
 
-class Server:
+    try:
 
-    while True:
-        clientsocket, address = s.accept()
-        print(f"Connection from {address} has been established !")
-        clientsocket.send(bytes("welcome to the server", "utf-8"))
+        messageHeader = clientSocket.recv(HEADER_LENGTH)
+        if not len(messageHeader):
+            return False
 
-if __name__ == '__main__':
-    Server()
+        messageLength = int(messageHeader.decode('utf-8').strip())
+        return {'header': messageHeader, 'data': clientSocket.recv(messagelength)}
+
+    except:
+        # socket.close()
+        return False
+
+def receiveMessage(clientSocket):
+
+    try:
+        messageHeader = clientSocket.recv(HEADER_LENGTH)
+
+        if not len(messageHeader):
+            return False
+        messageLength = int(messageHeader.decode('utf-8').strip())
+
+        return {'header': messageHeader, 'data': clientSocket.recv(messageLength)}
+
+    except:
+
+        return False
+
+while True:
+
+    readSockets, _, exceptionSockets = select.select(socketsList, [], socketsList)
+
+
+    # Iterate over notified sockets
+    for notifiedSocket in readSockets:
+        if notifiedSocket == s:
+
+            clientSocket, client_address = s.accept()
+
+            user = receiveMessage(clientSocket)
+
+            if user is False:
+                continue
+            socketsList.append(clientSocket)
+
+
+            clients[clientSocket] = user
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        else:
+
+            message = receiveMessage(notifiedSocket)
+
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notifiedSocket]['data'].decode('utf-8')))
+
+                socketsList.remove(notifiedSocket)
+
+                del clients[notifiedSocket]
+
+                continue
+
+            user = clients[notifiedSocket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            for clientSocket in clients:
+
+                if clientSocket != notifiedSocket:
+                    clientSocket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notifiedSocket in exceptionSockets:
+
+        socketsList.remove(notifiedSocket)
+        del clients[notifiedSocket]
+
